46d5c6a7507a3d946fd15acf0211244904ecdd1d
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-17 19:30:54 -0500

    Components Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { getStationById } from './api/satnogsAPI'; // Import the function
+import React from 'react';
+import Station from './components/station';
 
 function App() {
-  const [station, setStation] = useState(null); // Store the fetched station data
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const stationId = import.meta.env.VITE_GROUND_ID; // Replace with the desired station ID
-        const data = await getStationById(stationId); 
-        
-        setStation(data[0]); 
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []); 
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  } else {
-
-    return (
+  return (
+    <main>
       <div>
         <h1>SatNOGS Station Information</h1>
-  
-        <h2>Station Details:</h2>
-        <ul>
-          {Object.entries(station).map(([key, value]) => (
-              <li key={key}>
-                <b>{key}:</b> {JSON.stringify(value)}
-              </li>
-          ))}
-        </ul>
-        <img src={station.image} alt={`Station:${station.name}`} />
-  
-        {/* ... (rest of the component for observations) */}
+        <Station />
       </div>
-    );
-
-  }
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/station.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { getStationById } from './api/satnogsAPI';
+
+function Station() {
+  const [station, setStation] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const stationId = import.meta.env.VITE_GROUND_ID;
+        const data = await getStationById(stationId);
+        setStation(data[0]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h2>Station Details:</h2>
+        <ul>
+          {Object.entries(station).map(([key, value]) => (
+            <li key={key}>
+              <b>{key}:</b> {JSON.stringify(value)}
+            </li>
+          ))}
+        </ul>
+        <img src={station.image} alt={`Station: ${station.name}`} />
+      </div>
+    );
+  }
+}
+
+export default Station;
