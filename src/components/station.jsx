c49326c2a2ab3b946c902bd489f795060589424e
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-18 12:50:29 -0500

    Update station.jsx

--- src/components/station.jsx
+++ src/components/station.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { getStationById } from '../api/satnogsAPI';
 import { useTranslation } from 'react-i18next';
-import Badge from 'react-bootstrap/Badge'; // Import Badge component
-import moment from 'moment'; // Import moment.js for date formatting
+import Badge from 'react-bootstrap/Badge';
+import moment from 'moment';
 
 function Station() {
   const [station, setStation] = useState(null);
@@ -35,14 +35,6 @@ function Station() {
     return <div>{t('loading')}...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (isLoading) {
-    return <div>{t('loading')}...</div>;
-  }
-
   // Function to determine badge color based on status
   const getStatusBadgeVariant = (status) => {
     switch (status) {
@@ -58,47 +50,45 @@ function Station() {
   };
 
   return (
-    <div className="container"> {/* Main container */}
-      <div className="row mt-4"> {/* Top container */}
+    <div className="container">
+      <div className="row mt-4">
         <div className="col-md-12">
-          <div className="d-flex justify-content-between align-items-center"> {/* Flexbox for alignment */}
-            <Badge bg={getStatusBadgeVariant(station.status)}> {/* Status badge */}
+          <div className="d-flex justify-content-between align-items-center">
+            <Badge bg={getStatusBadgeVariant(station.status)} className="me-3">
               {station.id}
             </Badge>
-            <div>
-              <h1 className="display-4">{station.name}</h1> {/* Large heading */}
-              <p>{station.description}</p> {/* Move description to the top */}
-            </div>
+            <h1>{station.name}</h1>
           </div>
         </div>
       </div>
 
-      <div className="row mt-4"> {/* Content container */}
-        <div className="col-md-6"> {/* Left column */}
-          <ul className="list-unstyled">
-            <li className="mb-3 bg-light"> {/* Location with grey background */}
-              <h5>{t('location')}:</h5>
-              <ul className="list-unstyled">
-                <li><b>{t('latitude')}:</b> {station.lat}</li>
-                <li><b>{t('longitude')}:</b> {station.lng}</li>
-                <li><b>{t('altitude')}:</b> {station.altitude} m</li>
-              </ul>
-            </li>
-            <li className="mb-3 bg-light"> {/* Activity with grey background */}
-              <h5>{t('activity')}:</h5>
-              <ul className="list-unstyled">
-                <li><b>{t('lastSeen')}:</b> {moment(station.last_seen).format('LLL')}</li>
-                <li><b>{t('created')}:</b> {moment(station.created).format('LLL')}</li>
-              </ul>
-            </li>
-            <li className="mb-3"> {/* Observations */}
-              <h5>{t('observations')}:</h5>
-              <p>{station.observations}</p>
-            </li>
-          </ul>
+      <div className="row mt-4">
+        <div className="col-md-6">
+          <div className="card p-3 mb-3 rounded"> {/* Description box */}
+            <p>{station.description}</p>
+          </div>
+          <div className="card p-3 mb-3 rounded"> {/* Location box */}
+            <h5>{t('location')}:</h5>
+            <ul className="list-unstyled">
+              {/* ... (latitude, longitude, altitude) */}
+            </ul>
+          </div>
+          <div className="card p-3 mb-3 rounded"> {/* Activity box */}
+            <h5>{t('activity')}:</h5>
+            <ul className="list-unstyled">
+              {/* ... (last seen and created) */}
+            </ul>
+          </div>
+          <div className="card p-3 rounded"> {/* Observations box */}
+            <h5>{t('observations')}:</h5>
+            <p>{station.observations}</p>
+          </div>
         </div>
-        <div className="col-md-6"> {/* Right column */}
-          <img src={station.image} alt={`Station: ${station.name}`} className="img-fluid" />
+        <div className="col-md-6">
+          <img src={station.image} alt={`Station: ${station.name}`} className="img-fluid w-75" /> {/* Resized image */}
+          <div className="card p-3 mt-3 rounded"> {/* Bar with template text */}
+            <p>More details about the station, antenna specifications, and recent activities will be displayed here.</p>
+          </div>
         </div>
       </div>
     </div>
