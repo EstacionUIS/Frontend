caccc215081a54ad7d19a30766bba579f1607be3
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-18 12:07:21 -0500

    More format and localization

--- src/components/station.jsx
+++ src/components/station.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { getStationById } from '../api/satnogsAPI';
 import { useTranslation } from 'react-i18next';
+import Badge from 'react-bootstrap/Badge'; // Import Badge component
+import moment from 'moment'; // Import moment.js for date formatting
 
 function Station() {
   const [station, setStation] = useState(null);
@@ -31,21 +33,68 @@ function Station() {
 
   if (isLoading) {
     return <div>{t('loading')}...</div>;
-  } else {
-    return (
-      <div>
-        <h2>{t('stationDetails')}:</h2>
-        <ul>
-          {Object.entries(station).map(([key, value]) => (
-            <li key={key}>
-              <b>{key}:</b> {JSON.stringify(value)}
-            </li>
-          ))}
-        </ul>
-        <img src={station.image} alt={`Station: ${station.name}`} />
-      </div>
-    );
   }
+
+  // Function to determine badge color based on status
+  const getStatusBadgeVariant = (status) => {
+    switch (status) {
+      case 'Online':
+        return 'success';
+      case 'Offline':
+        return 'danger';
+      case 'Test':
+        return 'warning';
+      default:
+        return 'secondary'; 
+    }
+  };
+
+  return (
+    <div className="container"> {/* Main container */}
+      <div className="row mt-4"> {/* Top container */}
+        <div className="col-md-12">
+          <div className="d-flex justify-content-between align-items-center"> {/* Flexbox for alignment */}
+            <Badge bg={getStatusBadgeVariant(station.status)}> {/* Status badge */}
+              {station.id}
+            </Badge>
+            <div>
+              <h1 className="display-4">{station.name}</h1> {/* Large heading */}
+              <p>{station.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-4"> {/* Content container */}
+        <div className="col-md-6"> {/* Left column */}
+          <ul className="list-unstyled">
+            <li className="mb-3"> {/* Latitude, longitude, altitude */}
+              <h5>{t('location')}:</h5>
+              <ul className="list-unstyled">
+                <li><b>{t('latitude')}:</b> {station.lat}</li>
+                <li><b>{t('longitude')}:</b> {station.lng}</li>
+                <li><b>{t('altitude')}:</b> {station.altitude} m</li>
+              </ul>
+            </li>
+            <li className="mb-3"> {/* Last seen and created */}
+              <h5>{t('activity')}:</h5>
+              <ul className="list-unstyled">
+                <li><b>{t('lastSeen')}:</b> {moment(station.last_seen).format('LLL')}</li>
+                <li><b>{t('created')}:</b> {moment(station.created).format('LLL')}</li>
+              </ul>
+            </li>
+            <li> {/* Observations */}
+              <h5>{t('observations')}:</h5>
+              <p>{station.observations}</p>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-6"> {/* Right column */}
+          <img src={station.image} alt={`Station: ${station.name}`} className="img-fluid" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Station;
