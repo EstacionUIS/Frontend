1f21a5393079caeb43e7734ef3cb84448c7f3279
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-18 17:38:14 -0500

    Update about.jsx

--- src/components/Tabs/about.jsx
+++ src/components/Tabs/about.jsx
@@ -19,43 +19,43 @@ function About() {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <div className="bg-light rounded p-3"> {/* Add a container with rounded corners and padding */}
+          <div className="bg-light rounded p-3"> 
             <h2 className="mt-4">{t('about.title')}</h2> 
             <p>{t('about.description')}</p>
 
-            <ul className="list-unstyled"> {/* Use a list for better organization */}
+            <ul className="list-unstyled"> 
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Location />
                 </div>
               </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Installation />
                 </div>
-                </li>
+              </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Software />
                 </div>
-                </li>
+              </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Antenna />
                 </div>
-                </li>
+              </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Raspberry />
                 </div>
-                </li>
+              </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Power />
                 </div>
-                </li>
+              </li>
               <li>
-                <div className="bg-light rounded p-3">
+                <div className="bg-light rounded p-3 shadow-sm"> 
                   <Enclosure />
                 </div>
               </li>
