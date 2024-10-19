1b0ae89c570e3df2178ca12d0d923e2e7a831f85
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-19 14:39:39 -0500

    Update entry.jsx

--- src/components/Tabs/Observations/entry.jsx
+++ src/components/Tabs/Observations/entry.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Badge from 'react-bootstrap/Badge'; 
+import Badge from 'react-bootstrap/Badge';
 import { useTranslation } from 'react-i18next';
 
 function Entry({ observation }) {
@@ -7,30 +7,33 @@ function Entry({ observation }) {
 
     let badgeVariant;
     switch (observation.status) {
-    case 'future':
-        badgeVariant = 'primary'; // Blue
-        break;
-    case 'good':
-        badgeVariant = 'success'; // Green
-        break;
-    case 'bad':
-        badgeVariant = 'danger'; // Red
-        break;
-    case 'unknown':
-        badgeVariant = 'warning'; // Orange
-        break;
-    case 'failed':
-        badgeVariant = 'secondary'; // Grey
-        break;
-    default:
-        badgeVariant = 'secondary'; // Default to grey if status is not recognized
+        case 'future':
+            badgeVariant = 'primary';
+            break;
+        case 'good':
+            badgeVariant = 'success';
+            break;
+        case 'bad':
+            badgeVariant = 'danger';
+            break;
+        case 'unknown':
+            badgeVariant = 'warning';
+            break;
+        case 'failed':
+            badgeVariant = 'secondary';
+            break;
+        default:
+            badgeVariant = 'secondary';
     }
 
     return (
-    <li key={observation.id}>
-        {/* Display relevant observation details */}
-        <b>{t('Observations.Id')}:</b> {observation.id}, <Badge bg={badgeVariant}>{t(`Observations.Status.${observation.status}`)}</Badge>, <b>{t('Observations.TimeStamp')}:</b> {observation.start}
-    </li>
+        <li key={observation.id}>
+            {/* Display relevant observation details */}
+            <b>{t('Observations.Id')}:</b> {observation.id}, <Badge bg={badgeVariant}>{t(`Observations.Status.${observation.status}`)}</Badge>, <b>{t('Observations.TimeStamp')}:</b> {observation.start}<br />
+            <b>{t('Observations.Station')}:</b> {observation.station_name} ({observation.station_lat}, {observation.station_lng})<br />
+            <b>{t('Observations.Satellite')}:</b> {observation.norad_cat_id} - {observation.tle0}<br />
+            <b>{t('Observations.Transmitter')}:</b> {observation.transmitter_description} ({observation.transmitter_mode})
+        </li>
     );
 }
 
