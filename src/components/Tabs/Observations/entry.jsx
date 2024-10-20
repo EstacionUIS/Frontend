d97802a54e7b2b4fa142e9d83cdd6f0f5b851582
EstacionUIS <estacionterrenahosting@gmail.com>
2024-10-20 14:08:24 -0500

    Update entry.jsx

--- src/components/Tabs/Observations/entry.jsx
+++ src/components/Tabs/Observations/entry.jsx
@@ -3,36 +3,13 @@ import { useTranslation } from 'react-i18next';
 import Collapse from 'react-bootstrap/Collapse';
 import Card from 'react-bootstrap/Card';
 
-import { getSatelliteByNoradId } from '../../../api/satnogsAPI'; 
-
 import Header from './card/header'; 
 import Body from './card/body'; 
 
 function Entry({ observation }) {
-    const { t } = useTranslation();
     
+    const { t } = useTranslation();
     const [open, setOpen] = useState(false);
-    const [satelliteData, setSatelliteData] = useState(null);
-
-    useEffect(() => {
-        const fetchSatelliteData = async () => {
-            if (observation && observation.norad_cat_id) {
-                try {
-                    const data = await getSatelliteByNoradId(observation.norad_cat_id);
-                    setSatelliteData(data);
-                } catch (error) {
-                    console.error("Error fetching satellite data:", error);
-                    // Consider setting an error state to display to the user
-                }
-            }
-        };
-
-        fetchSatelliteData();
-    }, [observation]); 
-
-    if (!observation) {
-        return null; 
-    }
 
     return (
         <Card key={observation.id} className="mb-3">
