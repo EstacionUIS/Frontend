5dcc20e83147c9ccd5c561b9d7df35fb232780d7
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-20 15:18:39 -0500

    fix

--- src/components/Tabs/Observations/card/body.jsx
+++ src/components/Tabs/Observations/card/body.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Spinner from 'react-bootstrap/Spinner';
 
--- src/components/Tabs/Observations/entry.jsx
+++ src/components/Tabs/Observations/entry.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Collapse from 'react-bootstrap/Collapse';
 import Card from 'react-bootstrap/Card';
