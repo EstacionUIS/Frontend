e6bc80bf247a619dc5b99e9840aba629bd58699d
EstacionUIS <estacionterrenahosting@gmail.com>
2024-10-20 12:07:31 -0500

    Update header.jsx

--- src/components/Tabs/Observations/card/header.jsx
+++ src/components/Tabs/Observations/card/header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Badge from 'react-bootstrap/Badge';
 import { useTranslation } from 'react-i18next';
-import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 import moment from 'moment';
 
-function Header({ observation, open, setOpen }) {
+function Header({ observation }) { // Receive t function as prop
     const { t } = useTranslation();
 
     if (!observation) {
@@ -35,26 +34,19 @@ function Header({ observation, open, setOpen }) {
     const formattedDate = moment(observation.start).format('YYYY-MM-DD HH:mm:ss');
 
     return (
-        <Card.Header
-            onClick={() => setOpen(!open)}
-            aria-controls="observation-details"
-            aria-expanded={open}
-            style={{ cursor: 'pointer' }}
-        >
-            <div className="d-flex flex-column">
-                <div>
-                    <b>{t('Observations.Id')}:</b> {observation.id}
-                </div>
-                <div>
-                    <b>{t('Observations.TimeStamp')}:</b> {formattedDate}
-                </div>
-                <div>
-                    <Badge bg={badgeVariant}>
-                        {t(`Observations.Status.${observation.status}`)}
-                    </Badge>
-                </div>
+        <div className="d-flex flex-column">
+            <div>
+                <b>{t('Observations.Id')}:</b> {observation.id}
             </div>
-        </Card.Header>
+            <div>
+                <b>{t('Observations.TimeStamp')}:</b> {formattedDate}
+            </div>
+            <div>
+                <Badge bg={badgeVariant}>
+                    {t(`Observations.Status.${observation.status}`)}
+                </Badge>
+            </div>
+        </div>
     );
 }
 
