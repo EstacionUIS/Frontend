25cb606e5f5fec8b73c772f571293c68e56d0ee8
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-21 18:43:38 -0500

    Fixed entry.jsx

--- src/components/tabs/observations/entry.jsx
+++ src/components/tabs/observations/entry.jsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 import Collapse from 'react-bootstrap/Collapse';
 import Card from 'react-bootstrap/Card';
 import Spinner from 'react-bootstrap/Spinner';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'; 
 
 import Header from './components/header';
 
@@ -11,80 +13,78 @@ import Image from './cards/image';
 import Information from './cards/information';
 import Description from './cards/description';
 import Waterfall from './cards/waterfall';
-import Demoddata from './cards/demoddata';
+import Demoddata from './cards/demoddata'; 
 
 import { getSatelliteByNoradId } from '../../../api/satnogsAPI';
 
 function Entry({ observation }) {
+  if (!observation) {
+    return null;
+  }
 
-    if (!observation) {
-        return null;
-    }
+  const { t } = useTranslation();
 
-    const { t } = useTranslation();
- 
-    const [open, setOpen] = useState(false);
-    const [satelliteData, setSatelliteData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);    
+  const [open, setOpen] = useState(false);
+  const [satelliteData, setSatelliteData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const data = await getSatelliteByNoradId(observation.norad_cat_id);
-            setSatelliteData(data[0]);
-          } catch (error) {
-            setError(error);
-          } finally {
-            setIsLoading(false);
-          }
-        };
-    
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getSatelliteByNoradId(observation.norad_cat_id);
+        setSatelliteData(data[0]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    fetchData();
+  }, []);
 
-    return (
-        <Card key={observation.id} className="mb-3">
-        <Card.Header
-          onClick={() => setOpen(!open)}
-          aria-controls="observation-details"
-          aria-expanded={open}
-          style={{ cursor: 'pointer' }}
-        >
-          <Header observation={observation} />
-        </Card.Header>
-        <Collapse in={open}>
-          <Card.Body className="d-flex flex-column align-items-center justify-content-center">
-            {isLoading ? (
-              <div className="d-flex justify-content-center align-items-center vh-100">
-                <Spinner animation="border" role="status">
-                  <span className="visually-hidden">{t('Loading')}...</span>  
-  
-                </Spinner>
-              </div>
-            ) : (
-              <>
-                <Row className="mt-3" style={{ alignItems: 'stretch' }}> 
-                  <Col><Information satelliteData={satelliteData} /></Col>
-                  <Col><Image satelliteData={satelliteData} /></Col>
-                  <Col><Description satelliteData={satelliteData} /></Col>
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <Card key={observation.id} className="mb-3">
+      <Card.Header
+        onClick={() => setOpen(!open)}
+        aria-controls="observation-details"
+        aria-expanded={open}
+        style={{ cursor: 'pointer' }}
+      >
+        <Header observation={observation} />
+      </Card.Header>
+      <Collapse in={open}>
+        <Card.Body className="d-flex flex-column align-items-center justify-content-center">
+          {isLoading ? (
+            <div className="d-flex justify-content-center align-items-center vh-100">
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">{t('Loading')}...</span>
+              </Spinner>
+            </div>
+          ) : (
+            <>
+              <Row className="mt-3" style={{ alignItems: 'stretch' }}> 
+                <Col><Information satelliteData={satelliteData} /></Col>
+                <Col><Image satelliteData={satelliteData} /></Col>
+                <Col><Description satelliteData={satelliteData} /></Col>
+              </Row>
+              {observation.waterfall_status === "with-signal" && (
+                <Row className="mt-3">
+                  <Col><Waterfall observation={observation} /></Col>
+                  <Col><Demoddata observation={observation} /></Col>
                 </Row>
-                {observation.waterfall_status === "with-signal" && (
-                  <Row className="mt-3">
-                    <Col><Waterfall observation={observation} /></Col>
-                    <Col><Demoddata observation={observation} /></Col>
-                  </Row>
-                )}
-              </>
-            )}
-          </Card.Body>
-        </Collapse>
-      </Card>
-    );
-} 
+              )}
+            </>
+          )}
+        </Card.Body>
+      </Collapse>
+    </Card>
+  );
+}
 
 export default Entry;
