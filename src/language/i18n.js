1d2fa9ebdf39e4c8689ced08b40488adb445b164
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-21 15:27:51 -0500

    Update i18n.js

--- src/language/i18n.js
+++ src/language/i18n.js
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enTranslation from '/locales/en.json';
-import esTranslation from '/locales/es.json';
+import enTranslation from '../../public/locales/en.json';
+import esTranslation from '../../public/locales/es.json';
 
 i18n
   .use(LanguageDetector)
