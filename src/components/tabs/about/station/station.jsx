87d61ea05004ce0a477f3839f4075577014ee871
EstacionUIS <estacionterrenahosting@gmail.com>
2024-10-21 21:18:00 -0500

    Update station.jsx

--- src/components/tabs/about/station/station.jsx
+++ src/components/tabs/about/station/station.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import { Card, Collapse } from 'react-bootstrap';
 
+import { useTranslation } from 'react-i18next';
+
 function StationAbout() {
+
+  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
 
   return (
+    <div>
+      <h2 className="mt-4 text-center">{t('About.Intro.Title')}</h2> 
+      <p className="text-center">{t('About.Intro.Description')}</p>
+    </div>
     <Card>
-      <Card.Header onClick={() => setOpen(!open)} style={{ cursor: 'pointer' }}>
-        Click me to expand/collapse!
+      <Card.Header 
+        onClick={() => setOpen(!open)} 
+        style={{ cursor: 'pointer' }}
+        >
+        {t('About.StationProject')}
       </Card.Header>
       <Collapse in={open}>
         <Card.Body>
