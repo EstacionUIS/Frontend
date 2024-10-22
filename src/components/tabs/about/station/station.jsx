57defa9bcb1eb5ff818bbd9cd776a066744a2994
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-21 20:06:12 -0500

    Update station.jsx

--- src/components/tabs/about/station/station.jsx
+++ src/components/tabs/about/station/station.jsx
@@ -5,15 +5,15 @@ import Collapse from 'react-bootstrap/Collapse';
 import Card from 'react-bootstrap/Card';
 
 // Section
-import Location from './about/station/location';
-import Installation from './about/station/installation';
+import Location from '../station/location';
+import Installation from '../station/installation';
 
 // Collapsible
 //import Software from './about/station/collapsible/software';
-import Antenna from './about/station/collapsible/antenna';
+import Antenna from '../station/collapsible/antenna';
 /*import Raspberry from './about/station/collapsible/raspberry';*/
-import Power from './about/station/collapsible/power';
-import Enclosure from './about/station/collapsible/enclosure';
+import Power from '../station/collapsible/power';
+import Enclosure from '../station/collapsible/enclosure';
 
 function Station() {
 
