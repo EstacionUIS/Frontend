96c76fc331a525764911e0391c10643cca370c72
EstacionUIS <estacionterrenahosting@gmail.com>
2024-10-21 21:56:54 -0500

    Update innerworks.jsx

--- src/components/tabs/about/innerworks/innerworks.jsx
+++ src/components/tabs/about/innerworks/innerworks.jsx
@@ -1,39 +1,26 @@
 import React, { useState } from 'react';
+import { Card, Collapse } from 'react-bootstrap';
+
 import { useTranslation } from 'react-i18next';
-
-import Collapse from 'react-bootstrap/Collapse';
-import Card from 'react-bootstrap/Card';
-
-// Section
-import HoItWorks from '../innerworks/howitworks';
-
-// Collapsible
 
 function InnerWorks() {
 
-    const { t } = useTranslation();
-    const { open, setOpen } = useState(false);
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
 
   return (
     <div>
-      <div className='container roundedd p-4'>
+      <div>
         <h2 className="mt-4 text-center">{t('About.Intro.Title')}</h2> 
         <p className="text-center">{t('About.Intro.Description')}</p>
       </div>
       <Card>
-        <Card.Header
-          onClick={() => setOpen(!open)}
-          aria-controls="observation-details"
-          aria-expanded={open}
-          style={{ cursor: 'pointer' }}
-        >
+        <Card.Header onClick={() => setOpen(!open)} style={{ cursor: 'pointer' }}>
           {t('About.StationProject')}
         </Card.Header>
         <Collapse in={open}>
           <Card.Body>
-            <div>
-              <div><HoItWorks/></div>
-            </div>
+          Hello! I'm the content of the card.
           </Card.Body>
         </Collapse>
       </Card>
@@ -41,4 +28,4 @@ function InnerWorks() {
   );
 }
 
-export default InnerWorks;
+export default InnerWorkins;
