1b4c847992232779cec37a9c7692dc8e83b56829
digitalscales64 <digitalscalessynth@gmail.com>
2024-10-22 09:57:21 -0500

    Updated Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Header from './components/header';
+import Header from './components/header'; 
 import Contact from './components/header/contact';
 import Information from './components/header/information';
 import Body from './components/body';
 
 function App() {
-  return(
+  const [activeTab, setActiveTab] = useState('Home');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
     <div>
-      <div>
-        <Header />
-      </div>
-      <div>
-        <Contact />
-      </div>
-      <div>
-        <Information />
-      </div>
-      <div>
-        <Body />
-      </div>
+      <header> 
+        <Header activeTab={activeTab} onTabClick={handleTabClick} /> 
+      </header>
+
+      {activeTab === 'Home' && (
+        <main> 
+          <Contact />
+          <Information />
+          <Body />
+        </main>
+      )}
+      {activeTab === 'About' && <div>Content for About tab</div>} 
+      {activeTab === 'Station' && <div>Content for Station tab</div>}
+      {activeTab === 'Observations' && <div>Content for Observations tab</div>}
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,16 +5,31 @@ import logo2 from '/images/logo/logo2.png';
 
 import LanguageSelector from './header/languageSel';
 
-function Header() {
-
+function Header({ activeTab, onTabClick }) {
   return (
     <div>
       <div>
         <img src={logo1} alt="Logo 1" className="me-3" style={{ height: '50px' }} />
         <img src={logo2} alt="Logo 2" style={{ height: '50px' }} />
       </div>
+      <div> 
+        <ul> {/* Basic unordered list for tabs */}
+          <li onClick={() => onTabClick('Home')} className={activeTab === 'Home' ? 'active' : ''}>
+            Home
+          </li>
+          <li onClick={() => onTabClick('About')} className={activeTab === 'About' ? 'active' : ''}>
+            About
+          </li>
+          <li onClick={() => onTabClick('Station')} className={activeTab === 'Station' ? 'active' : ''}>
+            Station
+          </li>
+          <li onClick={() => onTabClick('Observations')} className={activeTab === 'Observations' ? 'active' : ''}>
+            Observations
+          </li>
+        </ul>
+      </div>
       <div>
-        <div><LanguageSelector /></div>
+        <LanguageSelector />
       </div>
     </div>
   );
