97efb90bb424a1b28e2b3413b05335f264dc799e
EstacionUIS <estacionterrenahosting@gmail.com>
2024-10-22 20:43:58 -0500

    Update home.jsx

--- src/components/routes/home.jsx
+++ src/components/routes/home.jsx
@@ -5,13 +5,21 @@ import Explanation from "./Home/explanation";
 import Facts from "./Home/facts";
 
 function Home() {
-    return (
-        <div>
-            <Station/>
-            <Explanation/>
-            <Facts/>
+  return (
+    <div className="container"> {/* Add a container for better structure */}
+      <div className="row"> {/* Use Bootstrap's grid system */}
+        <div className="col-md-12 text-center"> {/* Center content horizontally */}
+          <Station />
         </div>
-    );
+        <div className="col-md-12 text-center my-4"> {/* Add vertical margin */}
+          <Explanation />
+        </div>
+        <div className="col-md-12 text-center">
+          <Facts />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
